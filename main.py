52435d9d294986cb8fbf7d88053cd21db328bdb7
Alejandro <aavila0707@gmail.com>
2023-05-21 17:40:49 +0200

    refactor: add comments and function init

--- main.py
+++ main.py
@@ -1,53 +1,69 @@
 # python 3.8 (3.8.16) or it doesn't work
 # pip install streamlit streamlit-chat langchain python-dotenv
+import streamlit as st
+from streamlit_chat import message
+from dotenv import load_dotenv
+import os
 
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (
-    AIMessage,
+    SystemMessage,
     HumanMessage,
-    SystemMessage
-)
-from dotenv import load_dotenv
-import os
-import streamlit as st
-from streamlit_chat import message
-
-load_dotenv()
-
-# Load the OpenAI API key from the environment variable
-if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
-    print("OPENAI_API_KEY is not set")
-    exit(1)
-else:
-    print("OPENAI_API_KEY is set")
-
-
-chat = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-
-st.set_page_config(
-    page_title="ChatGPT",
-    page_icon="🤖",
-    layout="wide",
-    initial_sidebar_state="expanded",
+    AIMessage
 )
 
-st.header("Your own ChatGPT 🤖")
 
-if 'messages' not in st.session_state:
-    st.session_state['messages'] = [SystemMessage(
-        content="You are a helpful assistant.")]
-
-with st.sidebar:
-    user_input = st.text_input("You:", key='input')
-    if user_input:
-        st.session_state.messages.append(HumanMessage(content=user_input))
-        with st.spinner("Thinking..."):
-            response = chat(st.session_state.messages)
-        st.session_state.messages.append(AIMessage(content=response.content))
-
-messages = st.session_state.get('messages', [])
-for i, msg in enumerate(messages[1:]):
-    if i % 2 == 0:
-        message(msg.content, is_user=True, key=str(i) + '_user')
+def init():
+    # Load the OpenAI API key from the environment variable
+    load_dotenv()
+    
+    # test that the API key exists
+    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
+        print("OPENAI_API_KEY is not set")
+        exit(1)
     else:
-        message(msg.content, is_user=False, key=str(i) + '_ai')
+        print("OPENAI_API_KEY is set")
+
+    # setup streamlit page
+    st.set_page_config(
+        page_title="Your own ChatGPT",
+        page_icon="🤖"
+    )
+
+
+def main():
+    init()
+
+    chat = ChatOpenAI(temperature=0)
+
+    # initialize message history
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            SystemMessage(content="You are a helpful assistant.")
+        ]
+
+    st.header("Your own ChatGPT 🤖")
+
+    # sidebar with user input
+    with st.sidebar:
+        user_input = st.text_input("Your message: ", key="user_input")
+
+        # handle user input
+        if user_input:
+            st.session_state.messages.append(HumanMessage(content=user_input))
+            with st.spinner("Thinking..."):
+                response = chat(st.session_state.messages)
+            st.session_state.messages.append(
+                AIMessage(content=response.content))
+
+    # display message history
+    messages = st.session_state.get('messages', [])
+    for i, msg in enumerate(messages[1:]):
+        if i % 2 == 0:
+            message(msg.content, is_user=True, key=str(i) + '_user')
+        else:
+            message(msg.content, is_user=False, key=str(i) + '_ai')
+
+
+if __name__ == '__main__':
+    main()
